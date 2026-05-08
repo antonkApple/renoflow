6d4dd523f5e0e57972a7944a179304a55f084c0a
anton <antonk.apple@gmail.com>
2026-05-08 07:16:18 -0600

    first commit

---
+++ RenoFlow/RenoFlow/RenoFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  RenoFlowApp.swift
+//  RenoFlow
+//
+//  Created by Анна Кривчикова on 8.05.26.
+//
+
+import SwiftUI
+
+@main
+struct RenoFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
